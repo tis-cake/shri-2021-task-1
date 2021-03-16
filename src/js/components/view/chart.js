f479e73f68804f2092c7ad6ece503a66853a2033
tis <tisbootis@gmail.com>
2021-03-16 21:01:18 +0300

    add createChartTemplate for Chart slide

---
+++ src/js/components/view/chart.js
@@ -0,0 +1,115 @@
+import { cropExtension } from '../../utils/common';
+
+const CHART_MAX_HEIGHT = 70;
+
+const createLeaderMarkup = (leader, index) => {
+  const { name, avatar, valueText } = leader;
+
+  const placeNumber = index + 1;
+
+  const avatarCroppedName = cropExtension(avatar);
+
+  return (
+    `
+      <li class="chart__leader people__item">
+        <span class="people__img-wrap">
+          <picture>
+            <source
+              type="image/webp"
+              srcset="assets/images/1x/person/${avatarCroppedName}.webp 1x,
+                      assets/images/2x/person/${avatarCroppedName}.webp 2x,
+                      assets/images/3x/person/${avatarCroppedName}.webp 3x,
+                      assets/images/4x/person/${avatarCroppedName}.webp 4x"
+            >
+
+            <img
+              class="people__img"
+              src="assets/images/1x/person/${avatar}}"
+              srcset="assets/images/2x/person/${avatar} 2x,
+                      assets/images/3x/person/${avatar} 3x,
+                      assets/images/4x/person/${avatar} 4x"
+              alt="Место №${placeNumber}. ${name}"
+            >
+          </picture>
+        </span>
+        <span class="people__name">
+          ${name}
+        </span>
+        <span class="people__commit-count caption">
+          ${valueText}
+        </span>
+      </li>
+    `
+  );
+};
+
+const createListLeadersMarkup = (leaders) => {
+  return leaders
+    .map((el, i) => createLeaderMarkup(el, i))
+    .join('');
+};
+
+const createStatMarkup = (stat, maxValue) => {
+  const { title, value, active } = stat;
+
+  const activeClass = active
+    ? 'chart__stat chart__stat--gold'
+    : 'chart__stat';
+
+  const height = `${Math.floor((value * CHART_MAX_HEIGHT) / maxValue)}%`;
+
+  return (
+    `
+      <li class="${activeClass}" style="height: ${height}">
+        <p class="chart__amount-commits subtitle">${title}</p>
+        <p class="chart__sprint-number">${value}</p>
+      </li>
+    `
+  );
+};
+
+const createListStatsMarkup = (stats) => {
+  const statsReverse = [];
+  let maxValue = 0;
+
+  for (let i = 0; i < stats.length; i++) {
+    if (maxValue < stats[i].value) {
+      maxValue = stats[i].value;
+    }
+    statsReverse[i] = stats[(stats.length - 1) - i];
+  }
+
+  return statsReverse
+    .map((el) => createStatMarkup(el, maxValue))
+    .join('');
+};
+
+const createChartTemplate = (data) => {
+  const { title, subtitle, values, users } = data;
+
+  const listStatsMarkup = createListStatsMarkup(values);
+  const listLeadersMarkup = createListLeadersMarkup(users);
+
+  return (
+    `
+      <section class="chart container">
+        <div class="board__text-wrap chart__text-wrap">
+          <h1 class="board__title chart__title title">
+            ${title}
+          </h1>
+          <h2 class="board__subtitle chart__subtitle">
+            ${subtitle}
+          </h2>
+          <ul class="chart__list-stats">
+            ${listStatsMarkup}
+          </ul>
+          <ul class="chart__list-leaders people people--row">
+            ${listLeadersMarkup}
+          </ul>
+        </div>
+      </section>
+    `
+  );
+};
+
+export { createChartTemplate };
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import { sliderInit } from './components/slider';
 
 import { createLeadersTemplate } from './components/view/leaders';
 import { createVoteTemplate } from './components/view/vote';
+import { createChartTemplate } from './components/view/chart';
 
 import { renderMarkup } from './utils/render';
 
@@ -16,9 +17,11 @@ const board = document.querySelector('.board');
 // const leaders = createLeadersTemplate(DATA[5].data);
 // renderMarkup(board, leaders);
 
-// const vote = createVoteTemplate(DATA[2].data);
-const vote = createVoteTemplate(DATA[4].data);
-renderMarkup(board, vote);
+// const vote = createVoteTemplate(DATA[4].data);
+// renderMarkup(board, vote);
+// sliderInit('vote');
+
+const chart = createChartTemplate(DATA[7].data);
+renderMarkup(board, chart);
 
 colorThemeInit();
-sliderInit('vote');
