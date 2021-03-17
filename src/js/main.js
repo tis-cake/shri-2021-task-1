66d1824f48b5611341dcc62cef9715192c7c6d45
tis <tisbootis@gmail.com>
2021-03-17 21:17:50 +0300

    add createActivityTemplate for Activity slide

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import { createLeadersTemplate } from './components/view/leaders';
 import { createVoteTemplate } from './components/view/vote';
 import { createChartTemplate } from './components/view/chart';
 import { createDiagramTemplate } from './components/view/diagram';
+import { createActivityTemplate } from './components/view/activity';
 
 import { renderMarkup } from './utils/render';
 
@@ -18,14 +19,17 @@ const board = document.querySelector('.board');
 // const leaders = createLeadersTemplate(DATA[5].data);
 // renderMarkup(board, leaders);
 
-// const vote = createVoteTemplate(DATA[4].data);
-// renderMarkup(board, vote);
-// sliderInit('vote');
+const vote = createVoteTemplate(DATA[4].data);
+renderMarkup(board, vote);
+sliderInit('vote');
 
 // const chart = createChartTemplate(DATA[7].data);
 // renderMarkup(board, chart);
 
-const diagram = createDiagramTemplate(DATA[8].data);
-renderMarkup(board, diagram);
+// const diagram = createDiagramTemplate(DATA[8].data);
+// renderMarkup(board, diagram);
+
+// const activity = createActivityTemplate(DATA[10].data);
+// renderMarkup(board, activity);
 
 colorThemeInit();
